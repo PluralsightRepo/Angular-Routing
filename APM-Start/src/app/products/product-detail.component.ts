cc7ddf37d4ac6eeafc303add9e73a5fce15e7fa2
shirodkar-vishal <shirodkar-vishal@outlook.com>
2020-08-30 13:39:59 -0400

    module 5, 6 - Vishal S.

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Product } from './product';
-import { ProductService } from './product.service';
+import { Product, ProductResolved } from './product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: Product;
   errorMessage: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(private routeService: ActivatedRoute) { }
 
-  getProduct(id: number): void {
-    this.productService.getProduct(id).subscribe({
-      next: product => this.onProductRetrieved(product),
-      error: err => this.errorMessage = err
+  ngOnInit(): void {
+    this.routeService.data.subscribe(data => {
+      const ResolvedProduct: ProductResolved = data['resolvedProduct'];
+      this.product = ResolvedProduct.product;
+      this.errorMessage = ResolvedProduct.error;
     });
   }
 
--- APM-Start/src/app/shared/Routes/Routes.ts
+++ APM-Start/src/app/shared/Routes/Routes.ts
@@ -3,6 +3,7 @@ import { WelcomeComponent } from '../../home/welcome.component';
 import { ProductListComponent } from '../../products/product-list.component';
 import { ProductDetailComponent } from '../../products/product-detail.component';
 import { ProductEditComponent } from '../../products/product-edit/product-edit.component';
+import { ProductResolverService } from '../../products/product-resolver.service';
 import { LoginComponent } from '../../user/login.component';
 import { PageNotFoundComponent } from '../../page-not-found.component';
 
@@ -14,8 +15,14 @@ export const PrimaryRoutes: Route[] = [
 
 export const ProductRoutes: Route[] = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'products/:id/edit', component: ProductEditComponent },
+  { path: 'products/:id',
+    component: ProductDetailComponent,
+    resolve: {resolvedProduct: ProductResolverService }
+  },
+  { path: 'products/:id/edit',
+    component: ProductEditComponent,
+    resolve: {resolvedProduct: ProductResolverService }
+  },
 ];
 
 export const UserRoutes: Route[] = [
